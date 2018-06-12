f7da98ec853377afbc3a7d0f62a984ec80229ecb
vbox <saagrsaaml@gmail.com>
2018-06-12 17:58:08 +0545

    left right nav

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,98 @@
 <template>
   <div class="layout">
-    <Layout :style="{minHeight: '100vh'}">
-      <Sider collapsible :collapsed-width="78" v-model="isCollapsed">
-        <Menu mode="horizontal"  active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
-          <menu-item name="1-1">
-            <Icon type="ios-navigate"></Icon>
-            <span>Option 1</span>
-          </menu-item>
-          <menu-item name="1-2">
-            <Icon type="search"></Icon>
-            <span>Option 2</span>
-          </menu-item>
-          <menu-item name="1-3">
-            <Icon type="settings"></Icon>
-            <span>Option 3</span>
-          </menu-item>
+    <Layout>
+      <Header>
+        <Menu mode="horizontal" theme="dark" active-name="1">
+          <div class="layout-logo"></div>
+          <div class="layout-nav">
+            <MenuItem name="1">
+              <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '20px 20px 0'}" type="ios-navigate" size="24"></Icon>
+            </MenuItem>
+            <MenuItem name="2">
+              <Icon type="ios-keypad"></Icon>
+              Item 2
+            </MenuItem>
+            <MenuItem name="3">
+              <Icon type="ios-analytics"></Icon>
+              Item 3
+            </MenuItem>
+            <MenuItem name="4">
+              <Icon type="ios-paper"></Icon>
+              Item 4
+            </MenuItem>
+          </div>
         </Menu>
-      </Sider>
+      </Header>
       <Layout>
-        <Header :style="{background: '#fff', boxShadow: '0 2px 3px 2px rgba(0,0,0,.1)'}"></Header>
-        <Content :style="{padding: '0 16px 16px'}">
-          <Breadcrumb :style="{margin: '16px 0'}">
+        <Sider ref="side1" hide-trigger collapsible :collapsed-width="220" v-model="isCollapsed">
+          <Menu mode="vertical" active-name="1-2" theme="dark" width="auto" :open-names="['1']" :class="menuitemClasses">
+            <Submenu name="1">
+              <template slot="title">
+                <Icon type="ios-navigate"></Icon>
+                <span>Option 1</span>
+              </template>
+              <MenuItem name="1-1">Option 1</MenuItem>
+              <MenuItem name="1-2">Option 2</MenuItem>
+              <MenuItem name="1-3">Option 3</MenuItem>
+            </Submenu>
+            <Submenu name="2">
+              <template slot="title">
+                <Icon type="ios-keypad"></Icon>
+                <span>Option 1</span>
+              </template>
+              <MenuItem name="2-1">Option 1</MenuItem>
+              <MenuItem name="2-2">Option 2</MenuItem>
+            </Submenu>
+            <Submenu name="3">
+              <template slot="title">
+                <Icon type="ios-analytics"></Icon>
+                <span>Option 1</span>
+              </template>
+              <MenuItem name="3-1">Option 1</MenuItem>
+              <MenuItem name="3-2">Option 2</MenuItem>
+            </Submenu>
+          </Menu>
+        </Sider>
+        <Layout :style="{padding: '0 24px 24px'}">
+          <Breadcrumb :style="{margin: '24px 0'}">
             <BreadcrumbItem>Home</BreadcrumbItem>
             <BreadcrumbItem>Components</BreadcrumbItem>
             <BreadcrumbItem>Layout</BreadcrumbItem>
           </Breadcrumb>
-          <Card>
-            <div style="height: 600px">Content</div>
-          </Card>
-        </Content>
+          <Content :style="{padding: '24px', minHeight: '100vh', background: '#fff'}">
+            Content
+          </Content>
+        </Layout>
+        <Sider ref="side1" hide-trigger collapsible :collapsed-width="220" v-model="isCollapsed">
+          <Menu mode="vertical" active-name="1-2" theme="dark" width="auto" :open-names="['1']" :class="menuitemClasses">
+            <Submenu name="1">
+              <template slot="title">
+                <Icon type="ios-navigate"></Icon>
+                <span>Option 1</span>
+              </template>
+              <MenuItem name="1-1">Option 1</MenuItem>
+              <MenuItem name="1-2">Option 2</MenuItem>
+              <MenuItem name="1-3">Option 3</MenuItem>
+            </Submenu>
+            <Submenu name="2">
+              <template slot="title">
+                <Icon type="ios-keypad"></Icon>
+                <span>Option 1</span>
+              </template>
+              <MenuItem name="2-1">Option 1</MenuItem>
+              <MenuItem name="2-2">Option 2</MenuItem>
+            </Submenu>
+            <Submenu name="3">
+              <template slot="title">
+                <Icon type="ios-analytics"></Icon>
+                <span>Option 1</span>
+              </template>
+              <MenuItem name="3-1">Option 1</MenuItem>
+              <MenuItem name="3-2">Option 2</MenuItem>
+            </Submenu>
+          </Menu>
+        </Sider>
+
       </Layout>
     </Layout>
   </div>
@@ -42,11 +106,22 @@
       };
     },
     computed: {
+      rotateIcon () {
+        return [
+          'menu-icon',
+          this.isCollapsed ? 'rotate-icon' : ''
+        ];
+      },
       menuitemClasses: function () {
         return [
           'menu-item',
           this.isCollapsed ? 'collapsed-menu' : ''
         ]
+      }
+    },
+    methods: {
+      collapsedSider () {
+        this.$refs.side1.toggleCollapse();
       }
     }
 }
@@ -54,10 +129,6 @@
 
 <style>
 
-  .layout-con{
-    height: 100%;
-    width: 100%;
-  }
   .menu-item span{
     display: inline-block;
     overflow: hidden;
@@ -82,6 +153,9 @@
     transition: font-size .2s ease .2s, transform .2s ease .2s;
     vertical-align: middle;
     font-size: 22px;
+  }
+  .rotate-icon{
+    transform: rotate(-90deg);
   }
 
 </style>
